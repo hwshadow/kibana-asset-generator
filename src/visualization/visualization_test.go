4c7c650bd99ce69b4d680777411e883ffbb648ba
PGRE <hwshadow@gmail.com>
2017-08-21 12:02:01 -0700

    fixed visualization test

--- src/visualization/visualization_test.go
+++ src/visualization/visualization_test.go
@@ -35,18 +35,18 @@ var _ = Describe("Visualization", func() {
 					`terms<slice>(City)`,
 				}
 				results = Aggs{
-					Agg{ID: "0", Enabled: true, Type: "count", Schema: "metric", Params: nil},
-					Agg{ID: "1", Enabled: true, Type: "cardinality", Schema: "metric", Params: map[string]interface{}{"field": "City"}},
-					Agg{ID: "2", Enabled: true, Type: "avg", Schema: "metric", Params: map[string]interface{}{"field": "LowestBid"}},
-					Agg{ID: "3", Enabled: true, Type: "max", Schema: "metric", Params: map[string]interface{}{"field": "LowestBid"}},
-					Agg{ID: "4", Enabled: true, Type: "percentiles", Schema: "metric", Params: map[string]interface{}{"field": "coin", "percents": []string{"1", "3", "20", "100"}}},
-					Agg{ID: "5", Enabled: true, Type: "date_histogram", Schema: "segment", Params: map[string]interface{}{"field": "DateRequested", "customInterval": "2h", "interval": "auto", "min_doc_count": 1, "extended_bounds": map[string]interface{}{}}},
-					Agg{ID: "6", Enabled: true, Type: "filters", Schema: "segment", Params: map[string]interface{}{"filters": []map[string]interface{}{map[string]interface{}{"input": map[string]interface{}{"query": map[string]interface{}{"query_string": map[string]interface{}{"query": "all", "analyze_wildcard": true}}}, "label": "all"}}}},
-					Agg{ID: "7", Enabled: true, Type: "filters", Schema: "", Params: map[string]interface{}{"filters": []map[string]interface{}{map[string]interface{}{"input": map[string]interface{}{"query": map[string]interface{}{"query_string": map[string]interface{}{"query": "all", "analyze_wildcard": true}}}, "label": "all"}}}},
-					Agg{ID: "8", Enabled: true, Type: "filters", Schema: "split", Params: map[string]interface{}{"filters": []map[string]interface{}{map[string]interface{}{"input": map[string]interface{}{"query": map[string]interface{}{"query_string": map[string]interface{}{"query": "all", "analyze_wildcard": true}}}, "label": "all"}}}},
-					Agg{ID: "9", Enabled: true, Type: "terms", Schema: "segment", Params: map[string]interface{}{"field": "City", "size": 5, "order": "desc", "orderBy": "1"}},
-					Agg{ID: "10", Enabled: true, Type: "terms", Schema: "", Params: map[string]interface{}{"field": "City", "size": 5, "order": "desc", "orderBy": "1"}},
-					Agg{ID: "11", Enabled: true, Type: "terms", Schema: "", Params: map[string]interface{}{"field": "City", "size": 5, "order": "desc", "orderBy": "1"}},
+					Agg{ID: "0", Type: "count", Schema: "metric", Params: map[string]interface{}{}},
+					Agg{ID: "1", Type: "cardinality", Schema: "metric", Params: map[string]interface{}{"field": "City"}},
+					Agg{ID: "2", Type: "avg", Schema: "metric", Params: map[string]interface{}{"field": "LowestBid"}},
+					Agg{ID: "3", Type: "max", Schema: "metric", Params: map[string]interface{}{"field": "LowestBid"}},
+					Agg{ID: "4", Type: "percentiles", Schema: "metric", Params: map[string]interface{}{"field": "coin", "percents": []string{"1", "3", "20", "100"}}},
+					Agg{ID: "5", Type: "date_histogram", Schema: "segment", Params: map[string]interface{}{"field": "DateRequested", "customInterval": "2h", "interval": "auto", "min_doc_count": 1, "extended_bounds": map[string]interface{}{}}},
+					Agg{ID: "6", Type: "filters", Schema: "segment", Params: map[string]interface{}{"filters": []map[string]interface{}{map[string]interface{}{"input": map[string]interface{}{"query": map[string]interface{}{"query_string": map[string]interface{}{"query": "all", "analyze_wildcard": true}}}, "label": "all"}}}},
+					Agg{ID: "7", Type: "filters", Schema: "", Params: map[string]interface{}{"filters": []map[string]interface{}{map[string]interface{}{"input": map[string]interface{}{"query": map[string]interface{}{"query_string": map[string]interface{}{"query": "all", "analyze_wildcard": true}}}, "label": "all"}}}},
+					Agg{ID: "8", Type: "filters", Schema: "split", Params: map[string]interface{}{"filters": []map[string]interface{}{map[string]interface{}{"input": map[string]interface{}{"query": map[string]interface{}{"query_string": map[string]interface{}{"query": "all", "analyze_wildcard": true}}}, "label": "all"}}}},
+					Agg{ID: "9", Type: "terms", Schema: "segment", Params: map[string]interface{}{"field": "City", "size": 5}},
+					Agg{ID: "10", Type: "terms", Schema: "", Params: map[string]interface{}{"field": "City", "size": 5}},
+					Agg{ID: "11", Type: "terms", Schema: "", Params: map[string]interface{}{"field": "City", "size": 5}},
 				}
 			})
 
@@ -56,7 +56,6 @@ var _ = Describe("Visualization", func() {
 					agg, err = dsl.Parse(i)
 					Expect(err).ToNot(HaveOccurred())
 					Expect(agg.ID).To(Equal(results[i].ID))
-					Expect(agg.Enabled).To(Equal(results[i].Enabled))
 					Expect(agg.Type).To(Equal(results[i].Type))
 					Expect(agg.Params).To(Equal(results[i].Params))
 				}
