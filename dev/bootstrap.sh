3f9f7b260ac1043b0307cf31e0be0944de26acba
PGRE <hwshadow@gmail.com>
2017-08-21 17:53:52 -0700

    some light tom foolery

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -8,7 +8,7 @@ docker-compose up -d kibana-kag
 echo "\nSleep while containers get ready (6 seconds)"
 sleep 6
 echo "\nBuilding binary (default alpine)"
-#docker-compose run build-alpine-kag sh -c 'gb build'
-docker-compose run build-alpine-kag sh -c 'gb vendor restore && gb build'
-echo "\nBootstrap dnd test senario"
+docker-compose run build-alpine-kag sh -c 'gb build'
+#docker-compose run build-alpine-kag sh -c 'gb vendor restore && gb build'
+echo "\nBootstrap dnd test scenario"
 docker-compose run build-alpine-kag sh -c '/project/testdata/dnd/bootstrap.sh'
